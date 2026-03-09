e38c1b202d28db0a4b43c8b1e747d9738e2843f5
G Vignesh <g.vignesh@bcah.christuniversity.in>
2026-03-09 18:13:16 +0530

    fix

--- StockFilter.java
+++
@@ -1,5 +0,0 @@
-// Generic interface for filtering stock objects based on specific conditions
-public interface StockFilter<T> {
-    boolean meetsCriteria(T item);
-    String getFilterDescription();
-}
